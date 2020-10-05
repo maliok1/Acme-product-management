f4a6c1c1f7c0985fee917749777eb7e2e32797b9
Anastasia Mal <lisanastya15@gmail.com>
2020-10-05 15:41:51 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -8,6 +9,8 @@ import { ConvertToSpacesPipe } from './shared/converrt-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component';
+import {  ProductDetailGuard } from './products/product-detail.guard';
+import { WelcomeComponent } from './home/welcome.component';
 
 
 @NgModule({
@@ -17,12 +20,20 @@ import { ProductDetailComponent } from './products/product-detail.component';
     ConvertToSpacesPipe,
     StarComponent,
     ProductDetailComponent,
-    // ProductService
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id',canActivate: [ProductDetailGuard],
+       component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo:'welcome', pathMatch: 'full'},
+      {path: '**',  redirectTo:'welcome', pathMatch: 'full'}
+    ]),
   ],
   bootstrap: [AppComponent]
 })
